15ea387f04b6bd3c424238fde4dae8aab12e69de
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-03-13 16:56:12 +0000

    Grouping tutorials in main

---
+++ Go (Golang) Tutorial #3 - Variables, Strings & Numbers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	// strings
+	var nameOne string ="mario"
+	var nameTwo = "luigi"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "yoshi"
+	fmt.Println(nameFour)
+}
